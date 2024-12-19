cc0cb30bab4d85a113f3e596e8ba49f072be495c
dxxztoys <834830866@qq.com>
2024-12-19 17:38:54 +0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,36 @@
+enum PingUnit {
+    //% block="微妙"
+    MicroSeconds,
+    //% block="厘米"
+    Centimeters,
+    //% block="英寸"
+    Inches
+}
+
+/**
+ * Sonar and ping utilities
+ */
+//% color="#2c3e50" weight=24 icon="\uf101"
+//% blockId="sonar" block="超声波模块"
+namespace sonar {
+	//% color=#2c3e50 weight=24 icon="\uf101"
+    //% blockId=sonar_ping block="引脚 Trig %trig|Echo %echo|单位 %unit"
+    export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
+        // send pulse
+        pins.setPull(trig, PinPullMode.PullNone);
+        pins.digitalWritePin(trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(trig, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(trig, 0);
+
+        // read pulse
+        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
+
+        switch (unit) {
+            case PingUnit.Centimeters: return Math.idiv(d, 58);
+            case PingUnit.Inches: return Math.idiv(d, 148);
+            default: return d ;
+        }
+    }
+}
